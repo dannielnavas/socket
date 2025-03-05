ba6351704394ff010f0da1f93bdb2d324658a91c
Danniel Navas <dannielnavas@gmail.com>
2025-03-05 07:31:35 -0500

    ✨ Agregar pruebas para eventos y callbacks en Socket.IO

--- test/socket.test.js
+++ test/socket.test.js
@@ -26,4 +26,31 @@ describe("Testing socket.io", () => {
     io.close();
     clientSocket.close();
   });
+
+  test("should event", (done) => {
+    clientSocket.on("greeting", (greet) => {
+      try {
+        expect(greet).toBe("Welcome to socket.io");
+        done();
+      } catch (error) {
+        done(error);
+      }
+    });
+
+    serverSocket.emit("greeting", "Welcome to socket.io");
+  });
+
+  test("testing callbacks", (done) => {
+    serverSocket.on("bark", (callback) => {
+      callback("woof!");
+    });
+    clientSocket.emit("bark", (arg) => {
+      try {
+        expect(arg).toBe("woof!");
+        done();
+      } catch (error) {
+        done(error);
+      }
+    });
+  });
 });
